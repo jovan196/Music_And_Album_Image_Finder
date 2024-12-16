8b25e596220e165c1fae73a6ce7997b312955e94
jovan196 <19623032@std.stei.itb.ac.id>
2024-12-16 17:43:22 +0700

    fix: ESLint for frontend deployment

--- src/frontend_kenan/src/components/hasilbar.tsx
+++ src/frontend_kenan/src/components/hasilbar.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { useState, useEffect } from "react";
 import React from 'react';
 import axios from "axios";
--- src/frontend_kenan/src/components/sidebar.tsx
+++ src/frontend_kenan/src/components/sidebar.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import React, { useState } from 'react';
 import axios from 'axios';
 import HasilBar from './hasilbar';
